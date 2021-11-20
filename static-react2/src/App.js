cc0a4d0a8b3eda21e62601ad698f4879ceec6ab5
zac1182 <zac1182@gmail.com>
2021-11-20 16:08:16 +1300

    inital static page project

---
+++ static-react2/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div> <img width="50px" src={logo}/><br/>
+                <h1>Fun facts about React</h1>  
+                <ul>
+                    <li><b>Was first release in 2013</b></li>
+                    <li><b>Was originally created by ordan Walke</b></li>
+                    <li><b>Has well over 100k stars on Github</b></li>
+                    <li><b>Is maintained by FaceBook</b></li>
+                    <li><b>Powers thousands of enterprise apps, including mobile apps</b></li>
+                </ul>
+              </div>
+  );
+}
+
+export default App;
